e725c08732906fdc3910443fe53985b3f5eff747
Shalin Bhavsar <35373379+shalin24999@users.noreply.github.com>
2022-02-09 10:53:40 +0530

    Update app_verifier.py

--- app_verification/app_verifier.py
+++ app_verification/app_verifier.py
@@ -17,8 +17,8 @@ service_account_shuffler = os.environ['SERVICE_ACCOUNT_SHUFFLER']
 pr_number = os.environ['PR_NUMBER']
 print(username)
 #Creds for cloud function API
-json_account_info = json.loads(service_account_shuffler)
-credentials = service_account.Credentials.from_service_account_info(json_account_info)
+#json_account_info = json.loads(service_account_shuffler)
+credentials = service_account.Credentials.from_service_account_file('cred.json')
 service = build('cloudfunctions', 'v1',credentials=credentials)
 locations = service.projects().locations().list(name="projects/shuffler").execute()
 
